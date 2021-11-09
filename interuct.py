3c2ec655385afde75ef914669b4f58302c05a630
JeeRo0 <verginy@ya.ru>
2021-11-09 19:16:15 +0300

    less from book

--- interuct.py
+++ interuct.py
@@ -2,7 +2,7 @@ while True:
     reply = input('Введите текст:')
     if reply == 'стоп': break
     try:
-        print(int(reply)**2)
+        print(float(reply)**2)
     except:
         print('bad'*8)
 print('bye')
